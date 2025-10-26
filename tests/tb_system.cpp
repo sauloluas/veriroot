778d74d28551562f462e57b2706bcc0e6765e52a
Luis Meirelles <luis.gustavo.souto.meirelles@gmail.com>
2025-10-26 17:07:53 -0300

    cria o diretório `obj_dir/waves` caso não exista

--- tests/tb_system.cpp
+++ tests/tb_system.cpp
@@ -1,15 +1,27 @@
+#include <filesystem>
+
 #include "VTop.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
 int main() {
+  namespace fs = std::filesystem;
 
 	Verilated::traceEverOn(true);
 
 	VTop* top = new VTop;
 	VerilatedVcdC* vcd = new VerilatedVcdC;
 	top->trace(vcd, 99);
+<<<<<<< Updated upstream
 	vcd->open("obj_dir/waves/system.vcd");
+=======
+
+  if (!fs::exists("obj_dir/waves/")) {
+    fs::create_directories("obj_dir/waves/");
+  }
+
+	vcd->open("obj_dir/waves/waveform.vcd");
+>>>>>>> Stashed changes
 
 	top->clk = 0;
 
